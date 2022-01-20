0aaa501115b8d409e60f93be4d0436fc061edee1
alexey.anipchenko <alex.anipchenko@gmail.com>
2022-01-20 21:38:51 +0200

    Comment unused driver functionality

--- lib/DriveControl/DriveControl.h
+++ lib/DriveControl/DriveControl.h
@@ -1,21 +1,20 @@
 #include <Arduino.h>
-#include <analogWrite.h>
 
 /////////////////////////ENGINE CONTROL/////////////////////////
-#define COMMAND_ENGINE 1
-#define ENGINE_FORWARD 1
-#define ENGINE_BACKWARD 0
+// #define COMMAND_ENGINE 1
+// #define ENGINE_FORWARD 1
+// #define ENGINE_BACKWARD 0
 
 //---- you can change the values ​​of these pins for the convenience of connecting your motor driver ----
 
 //contacts responsible for the direction of rotation of the wheel.
 #define LEFT_FORWARD 12
-#define LEFT_BACKWARD 14
-#define RIGHT_FORWARD 27
-#define RIGHT_BACKWARD 26
+#define LEFT_BACKWARD 11
+// #define RIGHT_FORWARD 27
+// #define RIGHT_BACKWARD 26
 
 //PWM Arduino contacts responsible for motor power
-#define POWER_RIGHT 25
+// #define POWER_RIGHT 25
 #define POWER_LEFT 13
 
 //Driver control function
@@ -25,17 +24,17 @@ void Move(const byte &leftState, const byte &leftPower, const byte &rightState, 
   digitalWrite(LEFT_FORWARD, leftState);
   digitalWrite(LEFT_BACKWARD, !leftState);
 
-  analogWrite(POWER_RIGHT, rightPower);
-  digitalWrite(RIGHT_FORWARD, rightState);
-  digitalWrite(RIGHT_BACKWARD, !rightState);
+  // analogWrite(POWER_RIGHT, rightPower);
+  // digitalWrite(RIGHT_FORWARD, rightState);
+  // digitalWrite(RIGHT_BACKWARD, !rightState);
 }
 
 void InitialDriver()
 {
   //initialize the pins that control the driver
   pinMode(LEFT_FORWARD, OUTPUT);
-  pinMode(RIGHT_FORWARD, OUTPUT);
   pinMode(LEFT_BACKWARD, OUTPUT);
-  pinMode(RIGHT_BACKWARD, OUTPUT);
+  // pinMode(RIGHT_FORWARD, OUTPUT);
+  // pinMode(RIGHT_BACKWARD, OUTPUT);
   Move(0, 0, 0, 0);
 }
